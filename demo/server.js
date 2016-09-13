f0fac938ebd86d8a43dd5a952c6092514b514047
Mat Carey <mat@dorightdigital.com>
2016-09-13 22:12:21 +0100

    Update.

--- demo/server.js
+++ demo/server.js
@@ -1,7 +1,8 @@
 const express = require('express'),
   bodyParser = require('body-parser'),
   auth = require('../index').getClientForAuthServer('http://localhost:7809', process.env.APIKEY),
-  app = express();
+  app = express(),
+  bodyMiddleware = bodyParser.urlencoded({extended: true});
 
 const items = ['event', 'organiser', 'superadmin', 'user'];
 
@@ -14,6 +15,8 @@ function getLoginStatus(req) {
 app.use(auth.middleware());
 
 app.get('/user', function (req, res) {
+  "use strict";
+
   res.send(req.session || {noSession: true});
 });
 
@@ -23,7 +26,7 @@ app.get('/', (req, res) => {
   res.send([
     getLoginStatus(req),
     '<ul>',
-    items.map((i) => {
+    items.concat(['apikey']).map((i) => {
       return `<li><a href="${i}">${i}</a></li>`
     }).join(''),
     '</ul>'
@@ -44,10 +47,31 @@ items.forEach((i) => {
 ${description}</textarea><p><input type="submit" value="submit"/></p>`);
   });
 
-  app.post(editUrl, [auth.middleware(`${i}.update`), bodyParser.urlencoded()], function (req, res) {
+  app.post(editUrl, [auth.middleware(`${i}.update`), bodyMiddleware], function (req, res) {
     description = req.body.description;
     res.redirect('/');
   });
+});
+
+app.get('/apikey', (req, res) => {
+  "use strict";
+  const message = req.query.success === 'true' ? '<p>The provided API Key has been looked up and the results logged on the server (to avoid leaky security).</p>' : '';
+  res.send(`<h1>Lookup API Key</h1>${message}<form method="post" action="/apikey"><label>API Key <input name="apikey"/></label><input type="submit"/></form>`)
+});
+
+app.post('/apikey', [bodyMiddleware], (req, res) => {
+  "use strict";
+  const key = req.body.apikey;
+  auth.lookupApiKey(key)
+    .then(function (data) {
+      console.log('API Key: ', key);
+      console.log(JSON.stringify(data));
+      res.redirect(req.originalUrl.split('?')[0] + '?success=true');
+    })
+    .catch(function (err) {
+      res.send(`An error occurred: ${JSON.stringify(err.body)}`);
+    });
+
 });
 
 app.get('/login', (req, res) => {
@@ -55,10 +79,11 @@ app.get('/login', (req, res) => {
   if (req.user) {
     res.send(getLoginStatus(req));
   }
-  res.send('<form action="/login" method=post><label>Username or email: <input name="usernameOrEmail" value="test"/></label><label>Password: <input name="password" type="password" value="test"/></label><input type="submit" value="submit"/></form>')
+  const message = req.query.error ? `<h3 style="color: red;">${req.query.error}</h3>` : ''; // don't do this in production, it's a XSS attach vulnerability ... but fine for a basic demo.
+  res.send(`${message}<form action="/login" method=post><label>Username or email: <input name="usernameOrEmail" value="test"/></label><label>Password: <input name="password" type="password" value="test"/></label><input type="submit" value="submit"/></form>`)
 });
 
-app.post('/login', [bodyParser.urlencoded()], (req, res) => {
+app.post('/login', [bodyMiddleware], (req, res) => {
   "use strict";
   auth.login(req.body.usernameOrEmail, req.body.password, res)
     .then(function (data) {
@@ -66,18 +91,19 @@ app.post('/login', [bodyParser.urlencoded()], (req, res) => {
       res.send(`Logged in, try the <a href="/">Homepage</a>`);
     })
     .catch(function (err) {
-      console.log('Failed to log in', req.body.usernameOrEmail, req.body.password);
-      console.warn(err);
-      res.redirect('/login');
+      console.log('Failed to log in', req.body.usernameOrEmail, (req.body.password || '').length);
+      console.warn(err.name, err.message);
+      console.warn(err.stack);
+      res.redirect('/login?error=' + encodeURIComponent([err.name || 'error', err.message || ''].join(': ')));
     })
 });
 
-app.use(function errorHandler(err, req, res, next) {
+app.use((err, req, res, next) => {
   if (err.name === 'NoUser') {
     res.redirect('/login');
   } else if (err.name === 'ForbiddenUser') {
     res.status(403);
-    res.send('403 - Forbidden.  We know who you are and you\'re not allowed in.');
+    res.send('403 - Forbidden.  We know who you are and you\'re not allowed in.  ' + err.message);
   } else {
     console.error('unexpected error received', err);
     res.status(500).send('An unexpected error occurred, please try again.');
--- index.js
+++ index.js
@@ -1,30 +1,64 @@
 const request = require('mll-api-request-node'),
   cookieParser = require('cookie-parser'),
   _ = require('lodash'),
-  lastModifiedBySessionId = {};
+  authChainHeader = 'x-mll-auth-chain',
+  lastModifiedBySessionId = {},
+  lastModifiedByApiKey = {};
 
 function NoUser(message) {
   this.name = "NoUser";
-  this.message = (message || "");
+  this.message = (message || '');
 }
-NoUser.prototype = Error.prototype;
+NoUser.prototype = Object.create(Error.prototype);
+
+function IncorrectCredentials(message) {
+  this.name = "IncorrectCredentials";
+  this.message = (message || 'Incorrect Credentials Provided.');
+}
+IncorrectCredentials.prototype = Object.create(Error.prototype);
 
 function ForbiddenUser(message) {
   this.name = "ForbiddenUser";
-  this.message = (message || "");
+  this.message = (message || '');
 }
-ForbiddenUser.prototype = Error.prototype;
+ForbiddenUser.prototype = Object.create(Error.prototype);
 
 function LoginFailed(message, apiResponseBody) {
   this.name = "LoginFailed";
-  this.message = (message || "");
+  this.message = (message || '');
   this.apiResponseBody = (apiResponseBody || {});
 }
-LoginFailed.prototype = Error.prototype;
+LoginFailed.prototype = Object.create(Error.prototype);
+
+function BadRequest(message, apiResponseBody) {
+  this.name = "BadRequest";
+  this.message = (message || '');
+  this.apiResponseBody = (apiResponseBody || {});
+}
+BadRequest.prototype = Object.create(Error.prototype);
+
+function keyValueObject(key, value) {
+  const obj = {};
+  if (value !== undefined) {
+    obj[key] = value;
+  }
+  return obj;
+}
+
+function addToAuthChain(req, type, key) {
+  req.authChain = req.authChain ? (';' + req.authChain) : (req.header(authChainHeader) || '');
+  if (type && key) {
+    req.authChain = type + ':' + key + req.authChain;
+  }
+}
 
 module.exports = {
-  getClientForAuthServer: function (baseUrl, authKey) {
+  getClientForAuthServer: function (baseUrl, authKey, optionsIn) {
     "use strict";
+    const options = _.extend({}, optionsIn);
+    const useApiKeys = options.supportApiKeys !== false && options.supportOnlySessionKeys !== true;
+    const useSessionKeys = options.supportSessionKeys !== false && options.supportOnlyApiKeys !== true;
+    const logger = options.logger || _.noop;
     if (!baseUrl) {
       throw new Error('Base URL is required');
     }
@@ -33,6 +67,7 @@ module.exports = {
     }
     const auth = request.getForServer(baseUrl, {basicAuth: authKey}),
       self = {
+        AUTH_CHAIN_HEADER_NAME: authChainHeader,
         COOKIE_NAME: 'mll-session',
         logger: {
           error: console.error.bind(console),
@@ -41,10 +76,29 @@ module.exports = {
         errors: {
           LoginFailed: LoginFailed,
           NoUser: NoUser,
-          ForbiddenUser: ForbiddenUser
+          IncorrectCredentials: IncorrectCredentials,
+          ForbiddenUser: ForbiddenUser,
+          BadRequest: BadRequest
+        },
+        getAuthChainHeaderForRequest: function (req) {
+          return keyValueObject(authChainHeader, req.authChain || req.header(authChainHeader));
+        },
+        register: function (details, res) {
+          const userData = _.pick(details, ['username', 'email', 'fullName', 'greeting', 'password', 'roles']);
+          if (!userData.password || (!userData.username && !userData.email)) {
+            throw new BadRequest('Password and username/email are required when creating an account');
+          }
+          return auth.post('/user', {
+            action: 'create',
+            user: userData
+          })
+            .then(function () {
+              return self.login(userData.email || userData.username, userData.password, res);
+            });
         },
         login: function (usernameOrEmail, password, res) {
           return auth.post('/session', {
+            action: 'login',
             credentials: {
               usernameOrEmail: usernameOrEmail,
               password: password
@@ -58,23 +112,69 @@ module.exports = {
               return response.body;
             })
             .catch(function (err) {
-              throw new LoginFailed('Failed to login user', err.response.body);
+              if (err.response && err.response.body.noCredentialsMatch) {
+                throw new IncorrectCredentials();
+              }
+              throw new LoginFailed('Failed to login user', err.response && err.response.body);
             })
         },
-        lookupUserBySessionKey: function (sessionKey) {
+        logout: function (req, res) {
+          const sessionKey = req.session && req.session.sessionKey;
+          if (!sessionKey) {
+            return Promise.resolve();
+          }
+          return auth.delete('/session/' + encodeURIComponent(sessionKey))
+            .then(function () {
+              res.clearCookie(self.COOKIE_NAME);
+            });
+        },
+        lookupSession: function (sessionKey) {
           return new Promise(function (res, rej) {
             if (sessionKey) {
               var headers = {};
               var cachedData = lastModifiedBySessionId[sessionKey];
               if (cachedData) {
-                var date = cachedData.lastModified;
-                headers['if-modified-since'] = date;
+                headers['if-modified-since'] = cachedData.lastModified;
               }
 
               auth.get(['', 'session', encodeURIComponent(sessionKey)].join('/'), {}, headers)
                 .then(function (response) {
                   var result = response.body;
                   lastModifiedBySessionId[sessionKey] = {
+                    lastModified: response.headers['last-modified'],
+                    result: result
+                  };
+                  res(result);
+                })
+                .catch(function (err) {
+                  if (err.statusCode === 304) { //not really an error, just a cache hit
+                    return res(cachedData.result);
+                  }
+                  console.error('Error looking up session', err.stack);
+                  rej(err);
+                })
+            } else {
+              res(undefined);
+            }
+          });
+        },
+        lookupUserByApiKey: function (apiKey, authChain) {
+          return new Promise(function (res, rej) {
+            if (apiKey) {
+              var headers = {};
+              var cachedData = lastModifiedByApiKey[apiKey];
+              if (cachedData) {
+                headers['if-modified-since'] = cachedData.lastModified;
+              }
+              const headersToSend = _.extend(keyValueObject(authChainHeader, authChain), headers);
+              const fullAuthChain = encodeURIComponent(_.compact(['apikey:' + apiKey, authChain]).join(';'));
+
+              auth.get(['', 'authchain', fullAuthChain].join('/'), {}, headersToSend)
+                .then(function (response) {
+                  var result = _.extend({
+                    apikey: apiKey
+                  }, response.body);
+                  lastModifiedByApiKey[apiKey] = {
                     lastModified: response.headers['last-modified'],
                     result: result
                   };
@@ -104,14 +204,36 @@ module.exports = {
             });
           })
             .then(function (sessionKey) {
-              return self.lookupUserBySessionKey(sessionKey);
+              return self.lookupSession(sessionKey);
             });
+        },
+        lookupApiKeyFromRequest: function (req) {
+          console.log(req.header('authorization') || req);
+          const matches = (req.header('authorization') || '').match(/^[Bb]asic ([\w\-=]+)$/);
+          const key = matches && matches[1];
+          return self.lookupUserByApiKey(key, req.header(authChainHeader));
         },
         middleware: function (requiredPermissions) {
           const args = arguments;
 
           function noUserFound(next) {
-            requiredPermissions ? next(new NoUser('No user for session key')) : next();
+            if (!requiredPermissions) {
+              return next();
+            }
+
+            const messageParts = ['No user for'];
+            if (useSessionKeys) {
+              messageParts.push('session');
+              if (useApiKeys) {
+                messageParts.push('or');
+              }
+            }
+            if (useApiKeys) {
+              messageParts.push('API');
+            }
+
+            messageParts.push('key');
+            next(new NoUser(messageParts.join(' ')));
           }
 
           function attachSessionToRequest(req) {
@@ -119,16 +241,33 @@ module.exports = {
               return Promise.resolve();
             }
             if (req.noSession) {
-              throw NoUser('No session found.');
+              return Promise.reject(new NoUser('No session found.'));
             }
-            return self.lookupSessionFromRequest(req)
-              .then(function (response) {
-                if (!response) {
-                  req.noSession = true;
-                  throw NoUser('No session found.');
-                }
-                req.session = response.session;
-              });
+            if (useSessionKeys) {
+              return self.lookupSessionFromRequest(req)
+                .then(function (response) {
+                  if (!response) {
+                    req.noSession = true;
+                    throw NoUser('No session found.');
+                  }
+                  addToAuthChain(req, 'session', response.session.sessionKey);
+                  req.session = response.session;
+                });
+            }
+            if (useApiKeys) {
+              return self.lookupApiKeyFromRequest(req)
+                .then(function (response) {
+                  if (!response) {
+                    req.noSession = true;
+                    throw NoUser('No session found.');
+                  }
+                  addToAuthChain(req, 'apikey', response.apikey);
+                  req.session = response.authchain;
+                  req.session.roles = response.authchain.roles;
+                });
+            }
+            console.error('no option of looking up api or session key');
+            return Promise.reject();
           }
 
           return function (req, res, next) {
@@ -144,9 +283,9 @@ module.exports = {
                     return flatRequiredPermission;
                   }
                 }));
-                if (missingPermissions.length > 0) {
+                if (missingPermissions.length > 0 && missingPermissions.length === _.size(args)) {
                   const missingList = missingPermissions.join(', ');
-                  next(new ForbiddenUser('Missing permissions: ' + missingList));
+                  next(new ForbiddenUser('You must have one of these permissions: ' + missingList));
                 } else {
                   next();
                 }
@@ -156,9 +295,7 @@ module.exports = {
               });
           }
         }
-      }
-      ;
+      };
     return self;
   }
-}
-;
+};
